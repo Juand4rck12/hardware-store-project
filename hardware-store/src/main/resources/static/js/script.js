8f223a705d105146dbbb87432890c76cb0c71b33
Juan_Diego <juan3312orrego@gmail.com>
2025-08-09 15:01:11 -0500

    script para integracion con datatable añadido

--- hardware-store/src/main/resources/static/js/script.js
+++ hardware-store/src/main/resources/static/js/script.js
@@ -18,3 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+// DATATABLE
+document.addEventListener("DOMContentLoaded", function() {
+    // Si la tabla existe
+    if ($('#myTable').length) {
+        // Inicializa DataTables en la tabla con id "myTable"
+    $('#myTable').DataTable({
+        // Opciones de idioma en español
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/2.0.3/i18n/es-ES.json"
+        }
+        // Puedes agregar más opciones aquí, como orden, longitud de página, etc.
+    });
+    }
+});
